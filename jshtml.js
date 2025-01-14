15ac5073afb880fbc8093b6df92beb5fb656b182
Roshnisarkar <roshnisarkardev20@gmail.com>
2025-01-14 18:43:29 +0530

    newrepo

---
+++ jshtml.js
@@ -0,0 +1,486 @@
+
+// console.log('hello this is java script');
+// document.write("Hello world <br>");
+// document.write("Hello world <br>");
+// document.write("Hello world this is roshni <br>");
+
+// var a = "roshni";
+// document.write(a );
+// var h = 10;
+// var i = 20;
+// document.write("<br>" );
+// document.write(h+i);
+// document.write("<br>" );
+// document.write(20+30);
+//  var marks =10 ;
+//  if(marks>=100 ){
+// document.write("topper");
+//  }
+// else{
+//     document.write("fail");
+// }
+
+// var marks = 65;
+
+// if(marks==100 && marks>=70)
+// {
+//     document.write("pass");
+// }
+
+// else if(marks > 60 && marks < 40)
+// {
+//     document.write("ok");
+// }
+
+// else if(marks < 30 && marks>20)
+// {
+//     document.write("fail");
+// }
+
+// var lunch =["rosh", "eve","shivu"];
+// var guestname = prompt("what is name");
+// if lunch. includes(gustname)
+// {
+//     alert("welcome");
+// }
+// else{
+//     alert("soory try again");
+// }
+
+
+// var randomnumber1= Math.floor( Math.random () * 6 );
+// console.log var randomnumber1= Math.floor( Math.random () * 6 );
+
+
+
+// function roshni(){
+// console.log("hii rosh");
+
+
+
+// }
+
+// roshni(); //function call
+
+
+// var age = 20;
+// console.log(age);
+
+
+
+// test();
+
+// browser object model "B O M"
+
+// alert, conform, prompt
+
+
+// function roshni(){
+
+//    window.alert("this is a alert message")
+//    let abc= window.confirm("are you sure")
+// console.log(abc)
+
+// }
+
+// roshni()
+
+
+// function promptl(){
+//     let a= window.prompt("Heloo whats your name")
+//    alert("thanks for your feedback "+ a);
+   
+    
+// }
+
+// promptl()
+
+// division**********************
+
+// function condition(){
+   
+// let number=prompt("enter number")
+
+//    if(number%2==0){
+
+//     alert("even num");
+
+//    }
+
+//     else{
+//         alert("odd num");
+//     }
+
+//    }
+    
+
+
+// condition()
+
+// ask for two promt *********************************
+// add two numbers
+
+
+// function condition(){
+   
+// let number= parseFloat(prompt ("enter number"))
+
+//  let numbert=parseFloat(prompt("enter senond number"))
+//  let sum=number+numbert
+// console.log({sum})
+
+//    }
+
+//    condition()
+    
+// if else statement ****************************
+
+
+
+// Javascript Hw **************************
+
+// QUE 1. ternary oprator in js
+
+// function statement(){
+
+// var age =prompt("enter your age")
+// age >= 18 ? alert("eligible") : alert("wait to be 18")
+
+
+// }
+
+// if ok thankyou else saaad**********************
+
+// function statement(){
+// let t = confirm("feedback")
+// if(t==true){
+//     alert("thankyou")
+// }
+// else{
+//     alert("saad")
+// }
+
+
+// }
+
+// statement();
+
+
+
+
+// for loop print 1 to 10 *******************************************
+//  function statement(){
+
+
+// for(let a=1 ; a<=10 ; a++)
+// {
+//     console.log(a)
+// }
+
+// }
+
+// statement();
+
+// QUE 2.
+// function degree(){
+   
+    
+
+//     var a= confirm("have you complete your course");
+//     var b=confirm("have a cirtificate");
+// if ( a & b == true)
+//     {alert("complete")
+
+//     }
+//    else{
+//     alert("not complet")
+//    }
+    
+//         }
+    
+ 
+// degree()
+
+
+// QUE 3. discount
+// function degree(){
+   
+//     var disc;
+//     var a= parseFloat (prompt("enter price"));
+//     var b=confirm("have a copun");
+// if (  b == true)
+//     {
+//         disc= a-(a*0.1)
+//      console.log(disc )
+
+//     }
+   
+//         }
+    
+// degree()
+
+// QUE 4. marks pass/fail
+
+// function statement(){
+
+// var marks =prompt("enter your marks")
+// if(marks >= 35){
+//     console.log("pass")
+
+// }
+
+// else{
+//     console.log("fail")
+// }
+
+
+// }
+
+// statement();
+
+
+// QUE 5. check number is positive or negitive
+
+// function statement(){
+
+//     var number =prompt("enter your number")
+//     if(number >=0){
+//         console.log("positiv")
+    
+//     }
+    
+//     else{
+//         console.log("negetiv")
+//     }
+    
+    
+//     }
+    
+//     statement();
+    
+
+// QUE 6. odd even numbers **********************
+
+// function condition(){
+   
+// let number=prompt("enter number")
+
+//    if(number%2==0){
+
+//     alert("even num");
+
+//    }
+
+//     else{
+//         alert("odd num");
+//     }
+
+//    }
+
+// condition();
+
+
+
+
+// QUE 7. confirm to delete a file
+// function degree(){
+//     var del=confirm("delet this file");
+//     var delt=confirm("are you sure you want to delet this file");
+//     if(delt & del==true){
+//         alert("file is deleted")
+//     }
+
+//     else{
+//         alert("file is not deleted");
+//     }
+// }
+    
+// degree()
+
+// QUE 8. while loop need help
+
+// function statement(){
+
+//     var number = parseFloat (prompt("enter number"))
+//     while(number<=10){
+//     if(number <= 10){
+//        console.log ("try again")  
+    
+//     }
+    
+//     else{
+//       console.log("you  have entered number more then 10 you win this game")
+//     }
+// }
+    
+//     }
+    
+//     statement();
+
+
+
+// QUE 9. print 1-10 using while loop
+// function statement(){
+
+
+// let a=0
+// while(a<=10)
+// a++
+//     console.log("a")
+
+//     statement();
+
+
+
+//  QUE 10. check eligible age for voting
+// function statement(){
+
+// var age =prompt("enter your age")
+// if(age >= 18){
+//     console.log("eligible")
+
+// }
+
+// else{
+//     console.log("wait to be 18")
+// }
+
+
+// }
+
+// statement();
+// array----------------->>>>>>>>>>
+// var bro ="hello"
+// let rolno = 111
+// console.table([bro ,rolno]);
+
+// function for show after click on a button
+// function show(){
+// alert ("hello roshni");
+// console.log("hello Roshni");
+
+// }
+
+// document.write("<h1> hello Roshni</h1>");
+// document.write("Hello world <br>");
+
+// string lenght check>>>>>>>>>>>>>>>>>>>>
+// function stringcheck (){
+
+// let text= "this is string";
+// document.write(text.length);
+// }
+// stringcheck();
+
+// string slice check>>>>>>>>>>>>>>>>>>>> 
+
+// function stringslice (){
+
+//     let text= "thisisstring";
+//     document.write(text.slice(5,10));
+//     }
+//  stringslice ();
+
+// function stringslice (){
+// let low ="this is lower case"
+//     let text= "thisisstring";
+//     document.write(text.toUpperCase())
+//     document.write(low.toLowerCase())
+//     }
+//  stringslice ();
+
+
+// function stringslice (){
+
+// // let low ="this is lower case"
+//     let text= "thisisstring";
+//     let char = text.charAt(0);
+//     // String.charcodeAt(index)
+//     // document.write(text.toUpperCase())
+//     // document.write(low.toLowerCase())
+
+//     }
+
+//  stringslice ();
+
+
+// function func(){
+//     let text1="roshni"
+//     console.log(text1.charAt(4));
+//     console.log(text1.charCodeAt(3));
+//     console.log(text1.indexOf("n"));
+//     console.log(text1.split("h"));
+//     }
+//     func()
+// homework on click
+
+
+
+// function rosh(){
+
+//         document.write("this is a string")
+//       }
+
+
+// rosh();
+
+// stack & heap memory in JS------------>>>>>>>>
+
+
+// function str(){
+// alert("this is \"JS\" and \"JS\" is good")
+
+// }
+// str();
+
+// templet lettlers
+
+// supports multilines (``) and supports single qutes and double quuotes suppors hlmt tags dynamic vriables acan be use 
+// function str(){
+//     alert(`this is "JS" and "JS" 
+//         is good`)
+    
+//     }
+//     str();
+
+
+// test question >>>>>>>>>>>>>>>>>>>
+// DOM BOM
+// let var cont js introduction
+// hoisting string arry
+
+// arry javascript  ----------------->>>>>>>>>>>>>>>>>>>>
+//  lenght pop push shift unshift
+
+// function show(){
+//     let arry= ["ary","us","we"]
+//     console.log(arry.shift)
+//     arry.shift("orange")
+//     console.log(arry);
+    
+// }
+
+// conced arry ------------------->>>>>>>>>>>>>>>
+function arrying(){
+let arry = ["1","2", "3","4"]
+let arry1=["5","6", "7","8"]
+
+}
+arrying();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
